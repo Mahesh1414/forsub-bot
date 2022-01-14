100eaabcdc9b0512bffa4a84ce7d8609be6b943b
Mahesh1414 <89467357+Mahesh1414@users.noreply.github.com>
2022-01-14 09:42:40 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,27 +1,32 @@
 import os
 
 class Config():
-  ENV = bool(os.environ.get('ENV', False))
-  if ENV:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    DATABASE_URL = os.environ.get("DATABASE_URL", None)
-    APP_ID = os.environ.get("APP_ID", 6)
-    API_HASH = os.environ.get("API_HASH", None)
-  else:
-    BOT_TOKEN = "1979818055:AAFh8BenfeGWcgk5QaWgokTjbCPihT9X9ws"
-    DATABASE_URL = ""
-    APP_ID = "7903343"
-    API_HASH = "33e2c94e54db7dce047f572efa6f6679"
-
+  #Get it from @botfather
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  # Your bot updates channel username without @ or leave empty
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "bryllbots")
+  # Heroku postgres DB URL
+  DATABASE_URL = os.environ.get("DATABASE_URL", "")
+  # get it from my.telegram.org
+  APP_ID = os.environ.get("APP_ID", 123456)
+  API_HASH = os.environ.get("API_HASH", "")
+  # Sudo users( goto @JVToolsBot and send /id to get your id)
+  SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS", "1204927413 1405957830").split()))
+  SUDO_USERS.append(1204927413)
+  SUDO_USERS = list(set(SUDO_USERS))
 
 class Messages():
       HELP_MSG = [
         ".",
 
-        "**Force Subscribers**\nForce group members to join a specific channel before sending messages in the group.\n\n**Setup**\n**Step 1.** __Add me in a group(in which you are creator of the group) as admin.__\n**Step 2.** __Send__ `/ForceSubscribe {your channel username}`\n**Step 3.** __Add me to your channel as admin.__\n\n`All set ! I mute users who didn't joined your channel and ask them to join channel and unmute themself.`\n\n**Commands**\n\n/ForceSubscribe { off / no / disable} - To stop force subscriber\n\n/ForceSubscribe {Channel Username} - Set the Channel\n\n/ForceSubscribe - Get current Status",
+        "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
         
-        "**Developed by BRYLL CODERS**\n\nPowered by @bryllbots\nThanks to - BRYLL CODERS"
+        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
+        
+        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username or channel ID} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n\nNote: /FSub is an alias of /ForceSubscribe__",
+        
+       "**Devloped By @bryllbots**"
       ]
+      SC_MSG = "**Hey [{}](tg://user?id={})**\n click on below👇 button to get my source code, for more help ask in my support group👇👇 "
 
-      START_MSG = "Hey [{}](tg://user?id={})\nI am a multifunctional group manager bot from @bryllbots.\nLearn more at /help"
-      
+      START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /help__"
